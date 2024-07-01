75bafc0cc0e256c9d8870932439c5f0bf9d482ef
Siddharth <siddharth.das1@yahoo.com>
2024-06-30 23:21:05 -0700

    SPSA Tuned numerous parameters within the engine closer to optimal values at VSTC (5+0.05).
    Results of SkywallV0.6 vs SkywallV0.5:
    Elo: 52.09 +/- 12.75, nElo: 67.10 +/- 16.17
    LOS: 100.00 %, DrawRatio: 41.38 %, PairsRatio: 1.92
    Games: 1774, Wins: 803, Losses: 539, Draws: 432, Points: 1019.0 (57.44 %)
    Ptnml(0-2): [52, 126, 367, 190, 152]
    LLR: 2.89 (-2.25, 2.89) [0.00, 3.00]

--- Skywall/tunable.cpp
+++ Skywall/tunable.cpp
@@ -2,38 +2,38 @@
 
 #include "globals.h"
 
-TuneValue rfPruningBase("rfPruningBase", 95, 25, 150, 5);
+TuneValue rfPruningBase("rfPruningBase", 57, 25, 150, 5);
 TuneValue rfpDepth("rfpDepth", 5, 0, 10, 1);
 
 TuneValue nmpDepth("nmpDepth", 3, 0, 10, 1);
 TuneValue nmpBaseReduction("nmpBaseReduction", 3, 0, 10, 1);
 TuneValue nmpScaleReduction("nmpScaleReduction", 5, 1, 10, 1);
 
-TuneValue fpDepth("fpDepth", 8, 0, 12, 1);
-TuneValue fpScale("fpScale", 150, 30, 200, 10);
-TuneValue fpMargin("fpMargin", 0, 0, 360, 30);
+TuneValue fpDepth("fpDepth", 6, 0, 12, 1);
+TuneValue fpScale("fpScale", 141, 30, 200, 10);
+TuneValue fpMargin("fpMargin", 22, 0, 360, 30);
 
-TuneValue lmpDepth("lmpDepth", 5, 0, 12, 1);
-TuneValue lmpScale("lmpScale", 7, 1, 15, 2);
-TuneValue lmpBase("lmpBase", 2, 0, 10, 2);
+TuneValue lmpDepth("lmpDepth", 3, 0, 12, 1);
+TuneValue lmpScale("lmpScale", 8, 1, 15, 2);
+TuneValue lmpBase("lmpBase", 1, 0, 10, 2);
 
-TuneValue lmrBase("lmrBase", 77, 0, 200, 10);
-TuneValue lmrDivisor("lmrDivisor", 226, 100, 350, 10);
-TuneValue lmrDepth("lmrDepth", 2, 0, 12, 1);
-TuneValue lmrMoveCount("lmrMoveCount", 6, 0, 20, 1);
+TuneValue lmrBase("lmrBase", 73, 0, 200, 10);
+TuneValue lmrDivisor("lmrDivisor", 237, 100, 350, 10);
+TuneValue lmrDepth("lmrDepth", 1, 0, 12, 1);
+TuneValue lmrMoveCount("lmrMoveCount", 5, 0, 20, 1);
 
-TuneValue hstMin("hstMin", 1896, 200, 3000, 100);
+TuneValue hstMin("hstMin", 2025, 200, 3000, 100);
 TuneValue hstQuad("hstQuad", 4, 0, 10, 1);
-TuneValue hstLin("hstLin", 120, 0, 200, 10);
-TuneValue hstConst("hstConst", -120, -200, 200, 20);
+TuneValue hstLin("hstLin", 109, 0, 200, 10);
+TuneValue hstConst("hstConst", -100, -200, 200, 20);
 
-TuneValue hardTC("hardTC", 2, 1, 30, 1);
-TuneValue tcMul("tcMul", 60, 5, 100, 5);
+TuneValue hardTC("hardTC", 4, 1, 30, 1);
+TuneValue tcMul("tcMul", 48, 5, 100, 5);
 TuneValue timeMul("timeMul", 5, 1, 100, 3);
-TuneValue incMul("incMul", 75, 5, 100, 10);
+TuneValue incMul("incMul", 59, 5, 100, 10);
 
-TuneValue aspDelta("aspDelta", 70, 10, 150, 10);
-TuneValue aspWindow("aspWindow", 15, 3, 150, 5);
+TuneValue aspDelta("aspDelta", 84, 10, 150, 10);
+TuneValue aspWindow("aspWindow", 74, 3, 150, 5);
 
 
 vector<TuneValue *> allTunables = {
