da1ddacdd5b5fbeab280aed71c20593004344740
Siddharth <siddharth.das1@yahoo.com>
2024-08-15 18:02:00 -0700

    Skywall V0.7: SPSA Tuning LTC
    Tuned my parameters at LTC for 2070 iterations. End results are as follows.
    
    STC
    Elo: 16.69 +/- 8.04, nElo: 25.92 +/- 12.47
    LOS: 100.00 %, DrawRatio: 47.52 %, PairsRatio: 1.38
    Games: 2980, Wins: 1138, Losses: 995, Draws: 847, Points: 1561.5 (52.40 %)
    Ptnml(0-2): [68, 261, 708, 366, 87]
    LLR: 2.89 (-2.25, 2.89) [0.00, 5.00]
    
    Compared to SkywallV0.6:
    Elo difference: 226.4 +/- 44.5, LOS: 100.0 %, DrawRatio: 24.2 %
    SPRT: llr 2.97 (100.9%), lbound -2.94, ubound 2.94 - H1 was accepted

--- Skywall/tunable.cpp
+++ Skywall/tunable.cpp
@@ -2,71 +2,57 @@
 
 #include "globals.h"
 
-TuneValue rfPruningBase("rfPruningBase", 62, 25, 150, 5);
-TuneValue rfpDepth("rfpDepth", 5, 0, 10, 1);
-
-TuneValue nmpDepth("nmpDepth", 3, 0, 10, 1);
-TuneValue nmpBaseReduction("nmpBaseReduction", 3, 0, 10, 1);
-TuneValue nmpScaleReduction("nmpScaleReduction", 3, 1, 10, 1);
-
-TuneValue fpDepth("fpDepth", 7, 0, 12, 1);
-TuneValue fpScale("fpScale", 125, 30, 200, 10);
-TuneValue fpMargin("fpMargin", 10, 0, 360, 30);
-
-TuneValue lmpDepth("lmpDepth", 1, 0, 12, 1);
-TuneValue lmpQuad("lmpQuad", 4, 0, 100, 10);
-TuneValue lmpScale("lmpScale", 3, 1, 15, 2);
-TuneValue lmpBase("lmpBase", 2, 0, 10, 2);
-
-TuneValue hstPruneDepth("hstPruneDepth", 6, 0, 12, 1);
-TuneValue hstPruneScale("hstPruneScale", 2300, 0, 4000, 100);
-TuneValue hstPruneBase("hstPruneBase", 1000, -2000, 0, 100);
-
-TuneValue hstReduction("hstReduction", 8192, 0, 16683, 500);
-
-TuneValue lmrBase("lmrBase", 88, 0, 200, 10);
-TuneValue lmrDivisor("lmrDivisor", 246, 100, 350, 10);
-TuneValue lmrDepth("lmrDepth", 1, 0, 12, 1);
-TuneValue lmrMoveCount("lmrMoveCount", 5, 0, 20, 1);
-
-TuneValue hstMin("hstMin", 2008, 200, 3000, 100);
-TuneValue hstQuad("hstQuad", 5, 0, 10, 1);
-TuneValue hstLin("hstLin", 120, 0, 200, 10);
-TuneValue hstConst("hstConst", -126, -200, 200, 20);
-
-TuneValue qhstMin("qhstMin", 1620, 200, 3000, 20);
-TuneValue qhstQuad("qhstQuad", 6, 0, 10, 1);
-TuneValue qhstLin("qhstLin", 87, 0, 200, 5);
-TuneValue qhstConst("qhstConst", -111, -200, 200, 10);
-
-TuneValue hardTC("hardTC", 4, 1, 30, 1);
-TuneValue tcMul("tcMul", 50, 5, 100, 5);
-TuneValue timeMul("timeMul", 4, 1, 100, 3);
-TuneValue incMul("incMul", 66, 5, 100, 10);
-
-TuneValue aspDelta("aspDelta", 90, 10, 150, 10);
-TuneValue aspWindow("aspWindow", 80, 3, 150, 5);
-
-TuneValue iirDepth("iirDepth", 4, 0, 10, 1);
-
-TuneValue None("None", 0, 0, 0, 1);
-TuneValue mvvKing("mvvKing", 0, 0, 2000, 10);
-TuneValue mvvPawn("mvvPawn", 91, 0, 2000, 10);
-TuneValue mvvKnight("mvvKnight", 401, 0, 2000, 10);
-TuneValue mvvBishop("mvvBishop", 502, 0, 2000, 10);
-TuneValue mvvRook("mvvRook", 736, 0, 2000, 10);
-TuneValue mvvQueen("mvvQueen", 1192, 0, 2000, 10);
-
-TuneValue seeKing("seeKing", 0, 0, 2000, 10);
-TuneValue seePawn("seePawn", 108, 0, 2000, 10);
-TuneValue seeKnight("seeKnight", 446, 0, 2000, 10);
-TuneValue seeBishop("seeBishop", 428, 0, 2000, 10);
-TuneValue seeRook("seeRook", 665, 0, 2000, 10);
-TuneValue seeQueen("seeQueen", 1110, 0, 2000, 10);
-
-TuneValue seePDepth("seePDepth", 7, 0, 12, 1);
-TuneValue seeCPScale("seeCPScale", -100, -200, 0, 10);
-TuneValue seeQPScale("seeQPScale", -40, -200, 0, 10);
+TuneValue rfPruningBase("rfPruningBase", 61, 25.0, 150, 5);
+TuneValue rfpDepth("rfpDepth", 6, 0.0, 10, 1);
+TuneValue nmpDepth("nmpDepth", 1, 0.0, 10, 1);
+TuneValue nmpBaseReduction("nmpBaseReduction", 3, 0.0, 10, 1);
+TuneValue nmpScaleReduction("nmpScaleReduction", 3, 1.0, 10, 1);
+TuneValue fpDepth("fpDepth", 7, 0.0, 12, 1);
+TuneValue fpScale("fpScale", 124, 30.0, 200, 10);
+TuneValue fpMargin("fpMargin", 2, 0.0, 360, 30);
+TuneValue lmpDepth("lmpDepth", 2, 0.0, 12, 1);
+TuneValue lmpScale("lmpScale", 2, 1.0, 15, 2);
+TuneValue lmpQuad("lmpQuad", 3, 0.0, 100, 10);
+TuneValue lmpBase("lmpBase", 1, 0.0, 10, 2);
+TuneValue hstPruneDepth("hstPruneDepth", 6, 0.0, 12, 1);
+TuneValue hstPruneScale("hstPruneScale", 2109, 0.0, 4000, 100);
+TuneValue hstPruneBase("hstPruneBase", 1117, 0.0, 2000, 100);
+TuneValue hstReduction("hstReduction", 7413, 0.0, 16683, 500);
+TuneValue lmrBase("lmrBase", 92, 0.0, 200, 10);
+TuneValue lmrDivisor("lmrDivisor", 240, 100.0, 350, 10);
+TuneValue lmrDepth("lmrDepth", 1, 0.0, 12, 1);
+TuneValue lmrMoveCount("lmrMoveCount", 5, 0.0, 20, 1);
+TuneValue hstMin("hstMin", 2004, 200.0, 3000, 100);
+TuneValue hstQuad("hstQuad", 5, 0.0, 10, 1);
+TuneValue hstLin("hstLin", 111, 0.0, 200, 10);
+TuneValue hstConst("hstConst", -133, -200.0, 200, 20);
+TuneValue qhstMin("qhstMin", 1614, 200.0, 3000, 20);
+TuneValue qhstQuad("qhstQuad", 6, 0.0, 10, 1);
+TuneValue qhstLin("qhstLin", 90, 0.0, 200, 5);
+TuneValue qhstConst("qhstConst", -114, -200.0, 200, 10);
+TuneValue hardTC("hardTC", 2, 1.0, 30, 1);
+TuneValue tcMul("tcMul", 49, 5.0, 100, 5);
+TuneValue timeMul("timeMul", 7, 1.0, 100, 3);
+TuneValue incMul("incMul", 74, 5.0, 100, 10);
+TuneValue aspDelta("aspDelta", 105, 10.0, 150, 10);
+TuneValue aspWindow("aspWindow", 82, 3.0, 150, 5);
+TuneValue iirDepth("iirDepth", 3, 0.0, 10, 1);
+TuneValue None("None", 0, 0.0, 0, 1);
+TuneValue mvvKing("mvvKing", 7, 0.0, 2000, 10);
+TuneValue mvvPawn("mvvPawn", 89, 0.0, 2000, 10);
+TuneValue mvvKnight("mvvKnight", 412, 0.0, 2000, 10);
+TuneValue mvvBishop("mvvBishop", 502, 0.0, 2000, 10);
+TuneValue mvvRook("mvvRook", 742, 0.0, 2000, 10);
+TuneValue mvvQueen("mvvQueen", 1193, 0.0, 2000, 10);
+TuneValue seeKing("seeKing", 2, 0.0, 2000, 10);
+TuneValue seePawn("seePawn", 108, 0.0, 2000, 10);
+TuneValue seeKnight("seeKnight", 449, 0.0, 2000, 10);
+TuneValue seeBishop("seeBishop", 424, 0.0, 2000, 10);
+TuneValue seeRook("seeRook", 669, 0.0, 2000, 10);
+TuneValue seeQueen("seeQueen", 1096, 0.0, 2000, 10);
+TuneValue seePDepth("seePDepth", 7, 0.0, 12, 1);
+TuneValue seeCPScale("seeCPScale", -87, -200.0, 0, 10);
+TuneValue seeQPScale("seeQPScale", -38, -200.0, 0, 10);
 
 vector<TuneValue*> seeValues = {
 	&None,
